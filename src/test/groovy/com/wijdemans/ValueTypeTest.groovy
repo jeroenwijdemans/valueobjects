899ebda0849f5faf220813c3f40afb28869ea21b
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2017-05-24 18:12:39 +0200

    add more test cases

--- src/test/groovy/com/wijdemans/ValueTypeTest.groovy
+++ src/test/groovy/com/wijdemans/ValueTypeTest.groovy
@@ -4,10 +4,26 @@ import spock.lang.Specification
 
 class ValueTypeTest extends Specification {
 
+    def "i should be able to generate a new Id by using the utility method"() {
+        when:
+        Id id = Id.generate()
+
+        then:
+        id.toString().length() == 36
+    }
+
+    def "when i create a new object i expect the toString to be equal to its value"() {
+        when:
+        Id id = new Id("1")
+
+        then:
+        id.toString() == id.getValue()
+    }
+
     def "should equal as objects"() {
-        Id id1 = new Id("1");
-        Id id2 = new Id("1");
-        Id id3 = new Id("2");
+        Id id1 = new Id("1")
+        Id id2 = new Id("1")
+        Id id3 = new Id("2")
 
         expect:
         id1 == id2
@@ -15,11 +31,18 @@ class ValueTypeTest extends Specification {
     }
 
     def "should equal in set"() {
-        Id id1 = new Id("1");
-        Id id2 = new Id("1");
+        Id id1 = new Id("1")
+        Id id2 = new Id("1")
 
         expect:
         [id1] as Set == [id2] as Set
     }
 
+    def "when argument is null i expect an illegalargumentexception"() {
+        when:
+        new Id(null)
+
+        then:
+        thrown(IllegalArgumentException.class)
+    }
 }
