78335203384d424fb11021e0d08555c8495a28a2
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2017-05-30 07:31:34 +0200

    drop master check on deploy.sh since this will give us the tag value..

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'deploy ... '
-if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_5a8251e4ea42_key -iv $encrypted_5a8251e4ea42_iv -in ./cd/codesigning.asc.enc -out ./cd/codesigning.asc -d
     gpg --fast-import ./cd/codesigning.asc
     ./gradlew uploadArchives -s
